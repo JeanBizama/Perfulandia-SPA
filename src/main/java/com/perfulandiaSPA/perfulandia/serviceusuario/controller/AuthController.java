7916875ee49b5cbe3ca9459ba77bc41a8f4bfee1
Jean Bizama <jampybizama21@gmail.com>
2025-05-18 22:28:19 -0400

    basic login/register

---
+++ src/main/java/com/perfulandiaSPA/perfulandia/serviceusuario/controller/AuthController.java
@@ -0,0 +1,49 @@
+package com.perfulandiaSPA.perfulandia.serviceusuario.controller;
+
+import org.springframework.ui.Model;
+import com.perfulandiaSPA.perfulandia.serviceusuario.model.Usuario;
+import com.perfulandiaSPA.perfulandia.serviceusuario.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+
+@Controller
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @GetMapping("/login")
+    public String mostrarLogin() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String procesarLogin(@RequestParam String correo, @RequestParam String contraseña, Model model) {
+        Usuario usuario = usuarioService.findByCorreo(correo);
+        if (usuario.getContraseña().equals(contraseña)) {
+            model.addAttribute("error", "Credenciales correctas (aun no hay pagina de inicio)");
+            return "login";
+        }
+        else {
+            model.addAttribute("error", "Credenciales invalidas");
+            return "login";
+        }
+    }
+
+    @GetMapping("/register")
+    public String mostrarRegister(Model model) {
+        model.addAttribute("usuario", new Usuario());
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String procesarRegistro(@ModelAttribute("usuario") Usuario usuario) {
+        usuarioService.save(usuario);
+        return "redirect:/auth/login";
+
+    }
+
+}
